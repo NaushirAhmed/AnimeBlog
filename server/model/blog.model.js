62605ca585096b8356ac818187759a218ead2273
NaushirAhmed <naushirmdh786@gmail.com>
2026-09-11 21:45:15 +0530

    models

--- server/model/blog.model.js
+++ server/model/blog.model.js
@@ -1,5 +1,48 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const postSchema = new mongoose.Schema({
-    
+    title :{
+        type : String ,
+        required :true
+    },
+    image :{
+        type : String ,
+        required :true
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category'   
+    },
+    excerpt:{
+        type : String ,
+        required :true
+    },
+    author :{
+        type : String ,
+        required :true
+    },
+    trailerVideoId :{
+        type : String ,
+        required :true
+    },
+    trailerThumb:{
+        type : String ,
+        required :true
+    },
+    body: [
+        {
+            para1 :String,
+
+            
+        },
+        {
+            para2 : String
+        }
+      ],
+
+
+},{
+    timestamps :true
 })
+
+const Post = mongoose.model("Posts",postSchema);
